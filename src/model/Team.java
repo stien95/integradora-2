8bb357af71a37ad779ac93a5e36a131700636aee
Stien95 <stivenvillegas258@gmail.com>
2024-10-27 19:14:44 -0500

    changing coach to coach name and adding players list

--- src/model/Team.java
+++ src/model/Team.java
@@ -3,12 +3,13 @@ package model;
 public class Team {
     private String name;
     private String country;
-    private Person coach;
+    private String coachName;
+    private Player[] players;
 
-    public Team(String name, String country, Person coach) {
+    public Team(String name, String country, String coachName) {
         this.name = name;
         this.country = country;
-        this.coach = coach;
+        this.coachName = coachName;
     }
 
     public String getName() {
@@ -27,11 +28,41 @@ public class Team {
         this.country = country;
     }
 
-    public Person getCoach() {
-        return coach;
+    public String getCoachName() {
+        return coachName;
     }
 
-    public void setCoach(Person coach) {
-        this.coach = coach;
+    public void setCoachName(String coachName) {
+        this.coachName = coachName;
     }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
+
+    public String addPlayer(Player player) {
+        String message = "";
+        boolean added = false;
+        for (int i = 0; i < players.length && !added; i++) {
+            if (players[i] == null) {
+                players[i] = player;
+                added = true;
+                message = "Jugador agregado";
+            }
+        }
+        if (!added) {
+            message = "No se pueden agregar más jugadores";
+        }
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "    Nombre: " + name + "\n    País: " + country + "\n    Entrenador: " + coachName;
+    }
+
 }
